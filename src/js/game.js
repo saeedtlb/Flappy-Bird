05b00237d23a5bff04c383d01e4b0ce7068efdc8
saeed-tlb <saeedt7879s@gmail.com>
2020-11-29 16:46:02 +0330

    Hide menu section when user click the button and when he lose the menu will come back again so that user do not need to refresh page to play again. just click button and play as much as he/she wants

--- src/js/game.js
+++ src/js/game.js
@@ -6,9 +6,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const bird = document.querySelector('.bird');
     const game = document.querySelector('.game_container');
     const ground = document.querySelector('.ground_moving');
-    const scoreTag = document.querySelector('.score h2 span');
-    const bestscore = document.querySelector('.score h1 span');
+    const scoreTag = document.querySelectorAll('.score span');
+    const bestscore = document.querySelector('.best_score span');
+    const startbtn = document.querySelector('.menu .start_btn');
+    const menu = document.querySelector('.menu_container');
 
+    // Get user best score from local storage and show it in menu
     const best = window.localStorage.getItem('best_score');
     if (best) {
         bestscore.innerHTML = best;
@@ -17,31 +20,56 @@ document.addEventListener('DOMContentLoaded', () => {
     // logical varriables
     const speed = 40;
     const gravity = 3;
-    let isGameOver = false;
+    let isGameOver = true;
     let score = 0;
-    let { left: birdLeft, bottom: birdBottom } = getElementPosition(bird);
+    let { bottom: birdBottom } = getElementPosition(bird);
 
+    // Gravity of the game
     const startGame = () => {
         birdBottom -= gravity;
         bird.style.bottom = birdBottom + 'px';
     };
-    const gameTimer = setInterval(startGame, speed);
+
+    let gameTimer = null;
+    let time = null;
+
+    startbtn.addEventListener('click', () => {
+        console.log('restart');
+        // reset varriables
+        score = 0;
+        birdBottom = 240;
+        isGameOver = false;
+
+        scoreTag[0].innerHTML = score;
+
+        // remove obstacles
+        const obstacles = document.querySelectorAll('.obstacle');
+        obstacles.forEach(obstacle => game.removeChild(obstacle));
+
+        document.addEventListener('keyup', jump);
+
+        // start Game timers
+        gameTimer = setInterval(startGame, speed);
+        generateObstacle();
+        time = setInterval(generateObstacle, 4000);
+
+        menu.classList.add('hide');
+    });
 
     const jump = e => {
+        // If the key is space and the height of bird is less than 485 then increase height
         if (e.code === 'Space' && birdBottom < 485) birdBottom += 50;
         bird.style.bottom = birdBottom + 'px';
     };
 
-    document.addEventListener('keyup', jump);
-
     const generateObstacle = () => {
-        let obstacleLeft = 500;
-        let obstacleBottom = Math.random() * 60;
+        let obstacleLeft = 500; // First position of the obstacles
+        let obstacleBottom = Math.random() * 60; // Random num for height of obstacles
+
+        if (isGameOver) return;
 
         const obstacle = document.createElement('div');
         const obstacleTop = document.createElement('div');
-
-        if (isGameOver) return;
 
         obstacle.classList.add('obstacle');
         obstacleTop.classList.add('obstacle', 'obstacle_top');
@@ -55,8 +83,12 @@ document.addEventListener('DOMContentLoaded', () => {
         game.appendChild(obstacle);
         game.appendChild(obstacleTop);
 
+        // A function which will be call every 40ms(speed number) to decrease obstacleLeft = move obstacle to left
         const moveObstacles = () => {
-            if (isGameOver) return;
+            if (isGameOver) {
+                clearInterval(timer);
+                return;
+            }
 
             obstacleLeft -= 2;
             obstacle.style.left = obstacleLeft + 'px';
@@ -69,7 +101,7 @@ document.addEventListener('DOMContentLoaded', () => {
             }
             if (obstacleLeft === 170) {
                 score++;
-                scoreTag.innerHTML = score;
+                scoreTag[0].innerHTML = score;
             }
             if (
                 birdBottom === 0 ||
@@ -82,20 +114,23 @@ document.addEventListener('DOMContentLoaded', () => {
                 clearInterval(timer);
             }
         };
-        let timer = setInterval(() => moveObstacles(), speed);
-        if (!isGameOver) setTimeout(generateObstacle, 4000);
+        let timer = setInterval(moveObstacles, speed);
     };
-    generateObstacle();
 
     const gameOver = () => {
+        // stop timers
         clearInterval(gameTimer);
+        clearInterval(time);
+
         isGameOver = true;
         ground.classList.remove('ground_moving');
         document.removeEventListener('keyup', jump);
+        scoreTag[1].innerHTML = score;
+        menu.classList.remove('hide');
 
         if (score > best) {
             window.localStorage.setItem('best_score', score);
-            bestscore.innerHTML = best;
+            bestscore.innerHTML = score;
         }
     };
 });
