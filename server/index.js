5c2092dd1df3128a848a6e1f710466725955f16a
saeed-tlb <saeedt7879s@gmail.com>
2020-12-03 20:25:39 +0330

    send connected user positions to other players

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 const path = require('path');
 
 require('dotenv').config();
-
-const app = express();
 
 app.use(express.static(path.join(__dirname, '../', 'client')));
 
@@ -11,6 +12,21 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../', 'client', 'src', 'index.html'));
 });
 
-app.listen(process.env.PORT, () =>
+let users = 0;
+io.on('connection', socket => {
+    users++;
+    console.log(`users: ${users} connected`);
+
+    socket.on('position', data => {
+        socket.broadcast.emit('players', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+        users--;
+    });
+});
+
+http.listen(process.env.PORT, () =>
     console.log(`Server is up and running at port: ${process.env.PORT}`)
 );
