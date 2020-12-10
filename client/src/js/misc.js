068515d30bcaffc595a0e500d82dcd09eb0d25cf
saeed-tlb <saeedt7879s@gmail.com>
2020-12-10 13:24:58 +0330

    change calculateGap function and remove unwanted functions

--- client/src/js/misc.js
+++ client/src/js/misc.js
@@ -3,35 +3,5 @@ export const getElementPosition = (el, property) =>
 
 export const random = (min, max) => Math.random() * (max - min) + min;
 
-export const position = (bottom, gap) => {
-    let y1, y2;
-    if (bottom < 75) {
-        const chance = Math.random() <= 0.5;
-        y1 = chance ? bottom + gap : bottom;
-        y2 = !chance ? bottom + gap : bottom;
-    } else {
-        y1 = bottom;
-        y2 = bottom;
-    }
-
-    return { y1: -y1, y2: -y2 };
-};
-
-export const calculateGap = el => {
-    let gap = null;
-
-    const height = getElementPosition(el, 'height');
-    const width = getElementPosition(el, 'width');
-
-    if (height < 760) {
-        if (width <= 450) {
-            gap = 160;
-        } else {
-            gap = 100;
-        }
-    } else {
-        gap = 50;
-    }
-
-    return gap;
-};
+export const calculateGap = el =>
+    getElementPosition(el, 'height') < 760 ? 450 : 700;
